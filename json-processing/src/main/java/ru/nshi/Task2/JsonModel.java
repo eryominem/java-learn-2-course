5381f3f298acee93ae7d54ff8f28e121dad4d557
Maxim <maks.eryomin2013@yandex.ru>
2022-12-06 20:57:33 +0400

    few fix

--- json-processing/src/main/java/ru/nshi/Task2/JsonModel.java
+++ json-processing/src/main/java/ru/nshi/Task2/JsonModel.java
@@ -1,10 +1,13 @@
 package ru.nshi.Task2;
 
-import java.util.Arrays;
-
 public class JsonModel {
     private String algorithm;
     private int[] values;
+
+    public JsonModel(String algorithm, int[] values) {
+        this.algorithm = algorithm;
+        this.values = values;
+    }
 
     public String getAlgorithm() {
         return algorithm;
